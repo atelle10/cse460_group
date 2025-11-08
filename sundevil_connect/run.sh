9c6851e473642021c322ff706cc2c12c67c2ced0
Michael <mike@credencelimited.com>
2025-11-08 01:05:35 -0500

    migrations fixed

--- sundevil_connect/run.sh
+++ sundevil_connect/run.sh
@@ -9,10 +9,13 @@ fi
 
 source venv/bin/activate
 pip install -r requirements.txt
-rm -f db.sqlite3
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py shell < create_test_users.py
+
+if [ ! -f "db.sqlite3" ]; then
+    python3 manage.py makemigrations portal
+    python3 manage.py makemigrations core
+    python3 manage.py migrate
+    python3 manage.py shell < create_test_users.py
+fi
 echo ""
 echo "Setup done. Starting server..."
 echo ""
@@ -20,8 +23,6 @@ echo "Test Accounts created for grading:"
 echo "  Admin:    username: admin, password: admin123"
 echo "  Student: username: mkrasnik , password: pass123"
 echo "  Student: username: atelle10, password: pass123"
-echo ""
-echo "Ctrl+C to shut down the server..."
 echo ""
 
 python3 manage.py runserver
